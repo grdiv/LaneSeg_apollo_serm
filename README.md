2ebb8c9e400812b57d1c8841395df7b62f5ea336
66695 <2126997253@qq.com>
2023-01-02 00:36:05 +0800

    __init__

---
+++ README.md
@@ -0,0 +1,7 @@
+# LaneSeg_apollo
+Lane Segmentation on ApolloScapes dataset
+
+
+# train
+
+python train_no_dataloader.py --backbone resnet --resume pretrained_models/deeplab-resnet.pth.tar --ft false
---
+++ data_list/gen_data_list.py
@@ -0,0 +1,33 @@
+'''
+Author: dfs
+Date: 2023-01-01 21:31:38
+LastEditors: dfs
+LastEditTime: 2023-01-01 22:32:46
+Description: dfs
+'''
+
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+img_list = []
+train_list,val_list,test_list = [],[],[]
+
+img_data_root = '/media/dfs/Samsung_T5/dataset/SeRM/train'
+label_data_root = '/media/dfs/Samsung_T5/dataset/SeRM/trainannot'
+
+for img_name in os.listdir(img_data_root):
+    label_name = img_name.replace('image', 'annotation')
+    label_path = os.path.join(label_data_root, label_name)
+    img_path = os.path.join(img_data_root, img_name)
+    img_list.append([img_path, label_path])
+
+train_list, val_list = train_test_split(img_list, test_size=0.1, shuffle=True)
+
+train_part = pd.DataFrame(train_list, columns=['Image','Label'])
+val_part = pd.DataFrame(val_list, columns=['Image','Label'])
+
+train_part.to_csv('data_list/train_lite.csv')
+train_part.to_csv('data_list/val_lite.csv')
+
+pass
